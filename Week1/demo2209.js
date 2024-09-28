d5738b1558a795dd88c8b1da76fe22e034ed83c6
nxveen19 <pandeynaveen2002@gmail.com>
2024-09-29 00:17:36 +0530

    Add .gitignore file and ignore node_modules/, package.json, and package-lock.json
    Create Week1\01-Class.js file with class Animal and related functionalities
    
    Create Week1\demo2209.js file with various functions and examples
    
    Create Week1\demo2309.js file with additional functions and examples
    
    Create Week1\demo2409.js file with file reading and Promise examples
    
    Add .gitignore file and ignore node_modules/, package-lock.json, and package.json in Week2 directory
    
    Create Week2\demo2509.js file with an HTTP server and routes

---
+++ Week1/demo2209.js
@@ -0,0 +1,58 @@
+// starting today harkirat singh web dev course 01:02 pm 
+//simple primitives
+const prompt = require('prompt-sync')();
+let title = prompt("Enter title: ");
+console.log("hello world")   
+function greet(first_name, last_name){
+
+    if (title == "Mr."){
+    console.log("Hellooo Sir" +" " + first_name + " " +last_name +" You are Welcome");
+    }
+    else {
+        console.log("Hellooo Ma'am" +" " + first_name + " " +last_name +" You are Welcome");  
+    }
+}
+greet("Naveen", "Pandey");
+
+for (let i=0; i<10; i++){
+    console.log(i)
+}
+
+//complex primitives (array)
+//print even nos in an array
+let myArray = [1,2,3,4,5,6,7,8]
+let n = myArray.length
+for(i=myArray[0]; i<myArray[n-1]; i++){
+    if(i%2==0){
+    console.log(i)
+    }
+}
+//biggest number in an array
+for(j=myArray[0]; j<myArray[n-1]; j++){
+    if(j<j+1){
+    biggestNumber = j+1
+    }
+    console.log(biggestNumber)
+}
+//reverses all elements of a string
+function reverseArray(array){
+    let start = 0;
+    let end = array.length - 1;
+    while(start<end){
+    let temp = array[start];
+    array[start]  = array[end];
+    array[end] = temp;
+    start += 1;
+    end -=1;
+    }
+    return array;
+}
+console.log(reverseArray(myArray));
+
+const users = {
+    'name' : "NAveen",
+    surname : "Pandey"
+} 
+
+users.name = "anas"
+console.log(users)
