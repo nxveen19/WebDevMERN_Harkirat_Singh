52c3b717214b7e23ae3aed4030597ef17c37b40c
nxveen19 <pandeynaveen2002@gmail.com>
2025-01-17 18:47:46 +0530

    Add signup endpoint to create new users and check for existing usernames

--- Week3/demo1701.js
+++ Week3/demo1701.js
@@ -1,3 +1,6 @@
+const express = require("express"); 
+const app = express();
+app.use(express.json())
 const mongoose = require("mongoose");
 //connected with db user_app *NOT* with collection 
 mongoose.connect("mongodb+srv://naveen123:72769602@cluster0.ykxfp.mongodb.net/user_app") 
@@ -5,9 +8,24 @@ const User = mongoose.model('users', {
     name : String,   // this is the schema
     password: String
 });
-const user = new User({ name: 'Naveen pandey', 
-    email: 'naveen@gmail.com', password: '112233'}); // created new user
-user.save() //put data into database
-// kitty.save().then( () => console.log('meow'));
+
+app.post("/signup", async (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const name = req.body.name; 
+    // there is also user.update, user.delete which happens in mongodb, user is an obj here
+    const existingUser = await User.findOne({ email: username});
+    if (existingUser) {
+        return res.status(400).send("Username already exists");
+    }
+    const user = new User({ name: name, 
+        email: username, password: password
+    }); // created new user
+    user.save() //put data into database
+    res.json({
+        "msg": "User created successfully"
+    })
+    // kitty.save().then( () => console.log('meow')); 
+});
 
 // Step 2: Running this via http methods
