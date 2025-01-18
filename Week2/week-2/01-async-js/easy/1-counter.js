75d3b6ab3edce7a2b50804591c96c54dfb02d52c
nxveen19 <pandeynaveen2002@gmail.com>
2025-01-18 21:55:20 +0530

    Implement asynchronous file reading and expensive operation simulation in counter example

--- Week2/week-2/01-async-js/easy/1-counter.js
+++ Week2/week-2/01-async-js/easy/1-counter.js
@@ -1,7 +1,8 @@
 // We have already covered this in the second lesson, but as an easy recap try to code a counter in Javascript
 // It should go up as time goes by in intervals of 1 second
 
-
+let fs = require("fs");
+const { promise } = require("zod");
 function counter(duration){
     let count = 0;
 
@@ -21,12 +22,47 @@ intervalId = setInterval(() => {
 
 function counter1(){
     let count = 0;
-     function increment(){
+function increment(){
         count++;
         console.log(`count is ${count}`);
         setTimeout(increment, 1000)
-     }; 
-     increment();
+    }; 
+    increment();
     };
 // counter1()
+
+// Write code to read contents of a file and print it to the console. 
+// You can use the fs library to as a black box, the goal is to understand async tasks. 
+// Try to do an expensive operation below the file read and see how it affects the output. 
+// Make the expensive operation more and more expensive and see how it affects the output. 
+
+function expensiveOperation() {
+    let counter = 0;
+    const startTime = Date.now();
+    while (Date.now() - startTime < 7000) { // Simulate 5 seconds of work
+        counter++;
+    }
+    console.log(`Expensive operation completed. Counter: ${counter}`);
+}
+// The readFile callback still runs after the expensive operation because oprtn is asynchronous, 
+// but since the operation is synchronous, it still blocks the event loop until it's done.
+function navReadFile(){
+    let p = new Promise((resolve) => {
+        fs.appendFile("example.txt", "shuru krte hai bina kisi bkchodi ke", 'utf-8', (err) => { // append adds the new data while writeFile replaces with the existing data
+            console.log("data written successfully")
+        });
+        fs.readFile("example.txt", "utf-8", (err, data) => { // fs itself is async
+            resolve(data)
+        })
+
+    })
+    console.log("hii") // this wont ru n cause code after return wont execute
+    p
+    .then(data => {
+        console.log(data)
+    })
+    expensiveOperation()
+}
+navReadFile()
+
 
