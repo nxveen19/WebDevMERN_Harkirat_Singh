ee6b3cadd30747d9bedfc83911ac4c74806ea302
nxveen19 <pandeynaveen2002@gmail.com>
2025-02-15 18:13:17 +0530

    Add greeting and utility functions with TypeScript type inference

--- Week9/a.js
+++ Week9/a.js
@@ -1,3 +1,13 @@
 "use strict";
-const x = 1;
-console.log(x);
+function greet(firstName) {
+    console.log("hello" + firstName);
+}
+greet(" naveen");
+//type inference
+//ts infers a and b is number so sum is also number 
+function sum(a, b) {
+    return "gg";
+    //return a+b;
+}
+const value = sum(1, 2);
+console.log(value);
--- Week9/a.ts
+++ Week9/a.ts
@@ -1,2 +1,25 @@
-const x: number = 1;
-console.log(x)
+function greet(firstName: string){
+    console.log("hello" + firstName)
+}
+
+greet(" naveen");
+
+//type inference
+//ts infers a and b is number so sum is also number 
+function isLegal(age: number){
+    if (age> 18){
+        return true;
+    }else{
+        return false;
+    }
+}
+//islegal automatically inferred as boolean
+isLegal(12)
+
+//when parsing fn as an arg
+function runAfter1S(fn: () => void){
+    setTimeout(fn, 1000);
+}
+runAfter1S(function(){
+    console.log("hello world"); 
+})
